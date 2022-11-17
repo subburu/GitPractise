c84442fa330ba2ddf4bdaec0a79bd6ed30d735a4
subburusrava <subburusravan@gmail.com>
2022-11-17 18:23:53 +0530

    removed lines

--- src/com/git/GitClass.java
+++ src/com/git/GitClass.java
@@ -6,9 +6,7 @@ public class GitClass {
 	{
 		System.out.println("Sravan");
 		System.out.println("Git practise Testing123");
-		System.out.println("123");
-		System.out.println("1234");
-		System.out.println("12345");
+		
 	}
 
 }
