60d97fbdd3b6386883ed9c7e86ea8b841b80989a
subburusrava <subburusravan@gmail.com>
2022-11-17 18:17:09 +0530

    updated file

--- src/com/git/GitClass.java
+++ src/com/git/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Git practise Testing");
+		System.out.println("Sravan");
 		System.out.println("Git practise Testing123");
 		System.out.println("123");
 		System.out.println("1234");
